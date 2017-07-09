7b768c460115851307cda36b5895162555a22c2e
Andrew Fechner <arfechner@gmail.com>
2017-07-09 10:11:25 -0400

    Fix editing & deleting contact index issue when list is filtered

--- server/routes.js
+++ server/routes.js
@@ -30,9 +30,9 @@ module.exports = function(app, express) {
 
   // endpoint for making changes to contacts; route checks for existence of name before proceeding
   app.put('/edit', function(req,res) {
-    if (!contacts[req.body.name]) {
+    /*if (!contacts[req.body.name]) {
       utilities.responseBuilder(res,"There is no record with that name",req.body.name,null);
-    } else if (!Object.keys(req.body.data).length) {
+    } else */ if (!Object.keys(req.body.data).length) {
       utilities.responseBuilder(res,"You didn't change any information",req.body.name,contacts[req.body.name]);
     } else {
       utilities.editor(res,req.body.name,req.body.data);
